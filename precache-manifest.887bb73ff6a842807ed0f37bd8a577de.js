096770a0476be01c111c8b98bd29ee478a18e293
zhongjinyan <zhongjinyan@biad.com.cn>
2021-12-10 14:34:32 +0800

    add

---
+++ precache-manifest.887bb73ff6a842807ed0f37bd8a577de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be20bda9ffcc535427022516956e1e10",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37b3eb5807bacc1b3466",
+    "url": "/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "948df5a3a2219f4dc280",
+    "url": "/static/js/2.53aa29fd.chunk.js"
+  },
+  {
+    "revision": "a5db693410fed6a67f010a07b976fd8f",
+    "url": "/static/js/2.53aa29fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37b3eb5807bacc1b3466",
+    "url": "/static/js/main.a5244b81.chunk.js"
+  },
+  {
+    "revision": "949eb168f1d833ae83d5",
+    "url": "/static/js/runtime-main.743d20cc.js"
+  }
+]);
