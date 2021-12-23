6542834d2ed831a028c989d5143f6589087c9a22
zhongjinyan <zhongjinyan@biad.com.cn>
2021-12-23 11:03:35 +0800

    add

--- examples/nft_improved_worker/threejs_worker_gltf.js
+++ examples/nft_improved_worker/threejs_worker_gltf.js
@@ -71,7 +71,7 @@ function start( container, marker, video, input_width, input_height, canvas_draw
     // var camera = new THREE.Camera();
     camera.matrixAutoUpdate = false;
     var camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 1000);
-    camera.position.z = 400;
+    camera.position.z = 10;
 
     scene.add(camera);
 
